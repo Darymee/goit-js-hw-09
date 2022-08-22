2dbe0e924ab7484ce05e7a9a2b92a90cf771ea13
Darymee <danieledaniele3005@gmail.com>
2022-08-22 19:43:01 +0200

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onStartClick);
+refs.btnStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.btnStop.disabled = false;
+  refs.btnStart.disabled = true;
+  timerId = setInterval(() => {
+    let switchColor = getRandomHexColor();
+    refs.body.style.backgroundColor = switchColor;
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+}
