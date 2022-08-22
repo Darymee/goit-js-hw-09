96d1f108e76bff8d728631de4c5aa82b428a3aed
Darymee <danieledaniele3005@gmail.com>
2022-08-22 21:56:30 +0200

    task 2 start

---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  spanDays: document.querySelector('[data-days]'),
+  spanHours: document.querySelector('[data-hours]'),
+  spanMinutes: document.querySelector('[data-minutes]'),
+  spanSeconds: document.querySelector('[data-seconds]'),
+};
