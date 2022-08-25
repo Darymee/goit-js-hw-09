085b03bedc48af09e8186d1ac896daac33f756e6
Darymee <danieledaniele3005@gmail.com>
2022-08-25 23:39:24 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,29 +13,66 @@ const refs = {
 
 refs.btnStart.disabled = true;
 
-const TODAY = new Date();
-let SELECTED_DAY = new Date();
+const CURRENT_DAY = new Date();
+let SELECTED_DAY = 0;
+
+// const FUTURE_DAY = 2771448234050;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  parseDate: true,
   onClose(selectedDates) {
-    if (selectedDates[0] < TODAY) {
+    if (selectedDates[0] < CURRENT_DAY) {
       Notiflix.Notify.failure('Please choose a date in the future');
       // window.alert('Please choose a date in the future');
       refs.btnStart.disabled = true;
     } else {
       refs.btnStart.disabled = false;
-      selectedDates[0] = TODAY;
+      // selectedDates[0] = CURRENT_DAY;
       Notiflix.Notify.success('This time is right');
+      console.log(SELECTED_DAY);
+      console.log(dateStr);
+      SELECTED_DAY = dateStr;
+      return SELECTED_DAY;
+      // return selectedDates;
     }
+    // convertMs(msSum);
+    // console.log(convertMs(msSum));
   },
 };
 
+// let msSum = FUTURE_DAY - CURRENT_DAY;
+
+// console.log(msSum);
+
+// options.onClose();
+
+// let aaac = selectedDates;
+// console.log(aaac);
+
 flatpickr(refs.input, options);
 require('flatpickr/dist/themes/confetti.css');
+
+refs.btnStart.addEventListener('click', onStartBtnClick);
+
+// const timer = {
+//   start() {
+//     const DAY_NOW = Date.now();
+//     setInterval(() => {
+//       const ad = Date.now();
+//       console.log(ad - DAY_NOW);
+//     }, 1000);
+//   },
+// };
+
+// timer.start();
+
+function onStartBtnClick() {
+  refs.btnStart.disabled = true;
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -55,3 +92,27 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+function addLeadingZero(value) {
+  // padStart()
+}
+
+// function makeDeltaTime() {
+//   setInterval(() => {
+//     // console.log('new Date', SELECTED_DAY);
+//     // console.log('Date.now()', Date.now());
+//     let delta = SELECTED_DAY - Date.now();
+
+//     console.log(delta);
+//     convertMs(delta);
+//   }, 1000);
+// }
+
+// makeDeltaTime();
+
+// function makeTimerInterface() {
+//   refs.spanDays.textContent
+//   refs.spanHours.textContent
+//   refs.spanMinutes.textContent
+//   refs.spanSeconds.textContent
+// }
