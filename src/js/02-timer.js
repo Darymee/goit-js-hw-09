40e3472609278f981fdc7252aebacada564667ad
Darymee <danieledaniele3005@gmail.com>
2022-08-27 00:47:16 +0200

    add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -14,16 +14,15 @@ const refs = {
 refs.btnStart.disabled = true;
 
 const CURRENT_DAY = new Date();
-let SELECTED_DAY = 0;
-
-// const FUTURE_DAY = 2771448234050;
+let SELECTED_DAY = new Date();
+let timeDiffence = 0;
+let timerId = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  parseDate: true,
   onClose(selectedDates) {
     if (selectedDates[0] < CURRENT_DAY) {
       Notiflix.Notify.failure('Please choose a date in the future');
@@ -31,47 +30,32 @@ const options = {
       refs.btnStart.disabled = true;
     } else {
       refs.btnStart.disabled = false;
-      // selectedDates[0] = CURRENT_DAY;
       Notiflix.Notify.success('This time is right');
-      console.log(SELECTED_DAY);
-      console.log(dateStr);
-      SELECTED_DAY = dateStr;
-      return SELECTED_DAY;
-      // return selectedDates;
+      SELECTED_DAY = selectedDates[0];
     }
-    // convertMs(msSum);
-    // console.log(convertMs(msSum));
   },
 };
-
-// let msSum = FUTURE_DAY - CURRENT_DAY;
-
-// console.log(msSum);
-
-// options.onClose();
-
-// let aaac = selectedDates;
-// console.log(aaac);
 
 flatpickr(refs.input, options);
 require('flatpickr/dist/themes/confetti.css');
 
 refs.btnStart.addEventListener('click', onStartBtnClick);
 
-// const timer = {
-//   start() {
-//     const DAY_NOW = Date.now();
-//     setInterval(() => {
-//       const ad = Date.now();
-//       console.log(ad - DAY_NOW);
-//     }, 1000);
-//   },
-// };
-
-// timer.start();
-
 function onStartBtnClick() {
+  console.log(timeDiffence);
   refs.btnStart.disabled = true;
+  timerId = setInterval(() => {
+    timeDiffence = SELECTED_DAY - Date.now();
+    let convertTime = convertMs(timeDiffence);
+    if (timeDiffence <= 0) {
+      Notiflix.Notify.info('Time is over! Please reload the page');
+      clearInterval(timerId);
+    } else {
+      makeTimerInterface(convertTime);
+      refs.input.disabled = true;
+    }
+  }, 1000);
+  console.log(convertMs(timeDiffence));
 }
 
 function convertMs(ms) {
@@ -94,25 +78,12 @@ function convertMs(ms) {
 }
 
 function addLeadingZero(value) {
-  // padStart()
+  return String(value).padStart(2, '0');
 }
 
-// function makeDeltaTime() {
-//   setInterval(() => {
-//     // console.log('new Date', SELECTED_DAY);
-//     // console.log('Date.now()', Date.now());
-//     let delta = SELECTED_DAY - Date.now();
-
-//     console.log(delta);
-//     convertMs(delta);
-//   }, 1000);
-// }
-
-// makeDeltaTime();
-
-// function makeTimerInterface() {
-//   refs.spanDays.textContent
-//   refs.spanHours.textContent
-//   refs.spanMinutes.textContent
-//   refs.spanSeconds.textContent
-// }
+function makeTimerInterface(converTime) {
+  refs.spanDays.textContent = addLeadingZero(converTime.days);
+  refs.spanHours.textContent = addLeadingZero(converTime.hours);
+  refs.spanMinutes.textContent = addLeadingZero(converTime.minutes);
+  refs.spanSeconds.textContent = addLeadingZero(converTime.seconds);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+import flatpickr from 'flatpickr';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
