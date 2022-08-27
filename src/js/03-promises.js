32e59fa74ec707bbaa8ddaf72e83aafb597d99fb
Darymee <danieledaniele3005@gmail.com>
2022-08-27 21:25:44 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,43 @@
-import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
 };
 
-refs.form.addEventListener('submit', createPromise);
+refs.form.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(e) {
+  e.preventDefault();
+  let delayValue = Number(refs.form.delay.value);
+  let stepValue = Number(refs.form.step.value);
+  let amountValue = Number(refs.form.amount.value);
+
+  for (let i = 1; i <= stepValue; i += 1) {
+    createPromise(i, delayValue)
+      .then((position, delay) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch((position, delay) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += amountValue;
+  }
+}
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(position, delay);
+      } else {
+        reject(position, delay);
+      }
+    }, delay);
+  });
+  return promise;
 }
